12d365943609cf57cd7abe7f739fc1787da21faa
Djambax <98154375+Djambax@users.noreply.github.com>
2024-01-27 16:42:48 +0100

    Changement modal et ajustement js

--- app/models/Model.php
+++ app/models/Model.php
@@ -1,126 +1,126 @@
-<?php
-// app/models/Model.php
+    <?php
+    // app/models/Model.php
 
-require_once 'config.php'; // Ajustez le chemin selon votre structure de répertoire
+    require_once 'config.php'; // Ajustez le chemin selon votre structure de répertoire
 
-class Database {
-    private $conn;
+    class Database {
+        private $conn;
 
-    public function connect() {
-        $this->conn = null;
+        public function connect() {
+            $this->conn = null;
 
-        try {
-            $this->conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-            if ($this->conn->connect_error) {
-                throw new Exception("Connection failed: " . $this->conn->connect_error);
+            try {
+                $this->conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+                if ($this->conn->connect_error) {
+                    throw new Exception("Connection failed: " . $this->conn->connect_error);
+                }
+            } catch (Exception $exception) {
+                die("ERREUR : Impossible de se connecter. " . $exception->getMessage());
             }
-        } catch (Exception $exception) {
-            die("ERREUR : Impossible de se connecter. " . $exception->getMessage());
+
+            return $this->conn;
+        }
+    }
+
+
+
+
+
+    class QuizModel {
+        private $db;
+
+        public function __construct() {
+            $this->db = (new Database())->connect();
         }
 
-        return $this->conn;
-    }
-}
+        public function getAllQuizzes() {
+            $query = "SELECT quiz_id, title, description, image_path FROM quiz";
+            $result = $this->db->query($query);
 
+            if (!$result) {
+                return [];
+            }
 
+            $quizzes = [];
+            while ($quiz = $result->fetch_assoc()) {
+                $quizzes[] = $quiz;
+            }
 
-
-
-class QuizModel {
-    private $db;
-
-    public function __construct() {
-        $this->db = (new Database())->connect();
-    }
-
-    public function getAllQuizzes() {
-        $query = "SELECT quiz_id, title, description, image_path FROM quiz";
-        $result = $this->db->query($query);
-
-        if (!$result) {
-            return [];
+            return $quizzes;
         }
 
-        $quizzes = [];
-        while ($quiz = $result->fetch_assoc()) {
-            $quizzes[] = $quiz;
+        public function getQuizDetails($quiz_id) {
+            $stmt = $this->db->prepare("SELECT title, description, image_path FROM quiz WHERE quiz_id = ?");
+            $stmt->bind_param('i', $quiz_id);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            return $result->fetch_assoc();
+        }
+        public function getQuestionsByQuizId($quiz_id) {
+            $stmt = $this->db->prepare("SELECT question_id, question_text FROM questions WHERE quiz_id = ?");
+            $stmt->bind_param('i', $quiz_id);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            return $result->fetch_all(MYSQLI_ASSOC);
         }
 
-        return $quizzes;
-    }
+        public function getOptionsByQuestionId($questionId) {
+            $stmt = $this->db->prepare("SELECT option_id, option_text FROM options WHERE question_id = ?");
+            $stmt->bind_param('i', $questionId);
+            $stmt->execute();
+            return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+        }
 
-    public function getQuizDetails($quiz_id) {
-        $stmt = $this->db->prepare("SELECT title, description, image_path FROM quiz WHERE quiz_id = ?");
-        $stmt->bind_param('i', $quiz_id);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        return $result->fetch_assoc();
-    }
-    public function getQuestionsByQuizId($quiz_id) {
-        $stmt = $this->db->prepare("SELECT question_id, question_text FROM questions WHERE quiz_id = ?");
-        $stmt->bind_param('i', $quiz_id);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        return $result->fetch_all(MYSQLI_ASSOC);
-    }
+        public function checkIfOptionIsCorrect($questionId, $optionId) {
+            $stmt = $this->db->prepare("SELECT is_correct FROM options WHERE question_id = ? AND option_id = ?");
+            $stmt->bind_param('ii', $questionId, $optionId);
+            $stmt->execute();
+            return $stmt->get_result()->fetch_assoc()['is_correct'];
+        }
 
-    public function getOptionsByQuestionId($questionId) {
-        $stmt = $this->db->prepare("SELECT option_id, option_text FROM options WHERE question_id = ?");
-        $stmt->bind_param('i', $questionId);
-        $stmt->execute();
-        return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
-    }
+        public function getLeaderboardData($quizId) {
+            $leaderboard_query = "SELECT users.username, users.avatar_path, results.score FROM results JOIN users ON results.user_id = users.user_id WHERE results.quiz_id = ? ORDER BY results.score DESC, results.quiz_date ASC LIMIT 10";
+            $stmt = $this->db->prepare($leaderboard_query);
+            $stmt->bind_param('i', $quizId);
+            $stmt->execute();
+            return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+        }
 
-    public function checkIfOptionIsCorrect($questionId, $optionId) {
-        $stmt = $this->db->prepare("SELECT is_correct FROM options WHERE question_id = ? AND option_id = ?");
-        $stmt->bind_param('ii', $questionId, $optionId);
-        $stmt->execute();
-        return $stmt->get_result()->fetch_assoc()['is_correct'];
-    }
+        public function updateQuizScore($userId, $quizId, $newScore) {
+            // Vérifiez si un score existe déjà pour cet utilisateur et ce quiz
+            $check_score_sql = "SELECT result_id, score FROM results WHERE user_id = ? AND quiz_id = ?";
+            if ($check_stmt = $this->db->prepare($check_score_sql)) {
+                $check_stmt->bind_param('ii', $userId, $quizId);
+                $check_stmt->execute();
+                $check_result = $check_stmt->get_result();
 
-    public function getLeaderboardData($quizId) {
-        $leaderboard_query = "SELECT users.username, users.avatar_path, results.score FROM results JOIN users ON results.user_id = users.user_id WHERE results.quiz_id = ? ORDER BY results.score DESC, results.quiz_date ASC LIMIT 10";
-        $stmt = $this->db->prepare($leaderboard_query);
-        $stmt->bind_param('i', $quizId);
-        $stmt->execute();
-        return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
-    }
-
-    public function updateQuizScore($userId, $quizId, $newScore) {
-        // Vérifiez si un score existe déjà pour cet utilisateur et ce quiz
-        $check_score_sql = "SELECT result_id, score FROM results WHERE user_id = ? AND quiz_id = ?";
-        if ($check_stmt = $this->db->prepare($check_score_sql)) {
-            $check_stmt->bind_param('ii', $userId, $quizId);
-            $check_stmt->execute();
-            $check_result = $check_stmt->get_result();
-
-            if ($check_row = $check_result->fetch_assoc()) {
-                // Mise à jour du score existant si le nouveau score est supérieur
-                if ($newScore > $check_row['score']) {
-                    $update_score_sql = "UPDATE results SET score = ?, quiz_date = NOW() WHERE result_id = ?";
-                    if ($update_stmt = $this->db->prepare($update_score_sql)) {
-                        $update_stmt->bind_param('ii', $newScore, $check_row['result_id']);
-                        $update_stmt->execute();
-                        $update_stmt->close();
+                if ($check_row = $check_result->fetch_assoc()) {
+                    // Mise à jour du score existant si le nouveau score est supérieur
+                    if ($newScore > $check_row['score']) {
+                        $update_score_sql = "UPDATE results SET score = ?, quiz_date = NOW() WHERE result_id = ?";
+                        if ($update_stmt = $this->db->prepare($update_score_sql)) {
+                            $update_stmt->bind_param('ii', $newScore, $check_row['result_id']);
+                            $update_stmt->execute();
+                            $update_stmt->close();
+                        }
+                    }
+                } else {
+                    // Insertion d'un nouveau score
+                    $insert_score_sql = "INSERT INTO results (user_id, quiz_id, score, quiz_date) VALUES (?, ?, ?, NOW())";
+                    if ($insert_stmt = $this->db->prepare($insert_score_sql)) {
+                        $insert_stmt->bind_param('iii', $userId, $quizId, $newScore);
+                        $insert_stmt->execute();
+                        $insert_stmt->close();
                     }
                 }
+                $check_stmt->close();
             } else {
-                // Insertion d'un nouveau score
-                $insert_score_sql = "INSERT INTO results (user_id, quiz_id, score, quiz_date) VALUES (?, ?, ?, NOW())";
-                if ($insert_stmt = $this->db->prepare($insert_score_sql)) {
-                    $insert_stmt->bind_param('iii', $userId, $quizId, $newScore);
-                    $insert_stmt->execute();
-                    $insert_stmt->close();
-                }
+                // Gérer une erreur de préparation de la requête
+                echo "Erreur lors de la préparation de la requête pour vérifier ou mettre à jour le score.";
             }
-            $check_stmt->close();
-        } else {
-            // Gérer une erreur de préparation de la requête
-            echo "Erreur lors de la préparation de la requête pour vérifier ou mettre à jour le score.";
         }
-    }
 
-}
+    }
 
 
 
--- public/js/scene3d.js
+++ public/js/scene3d.js
@@ -27,6 +27,7 @@ document.addEventListener('DOMContentLoaded', function () {
     scene.add(mesh2);
 
     var INTERSECTED;
+
     function onMouseMove(event) {
         var mouse = new THREE.Vector2();
         var raycaster = new THREE.Raycaster();
@@ -37,20 +38,27 @@ document.addEventListener('DOMContentLoaded', function () {
         raycaster.setFromCamera(mouse, camera);
         var intersects = raycaster.intersectObjects(scene.children);
 
-        if (intersects.length > 0) {
-            if (INTERSECTED != intersects[0].object) {
+        if (!isModalOpen) {
+            if (intersects.length > 0) {
+                if (INTERSECTED != intersects[0].object) {
+                    if (INTERSECTED) INTERSECTED.scale.set(1, 1, 1);
+                    INTERSECTED = intersects[0].object;
+                    INTERSECTED.scale.set(1.2, 1.2, 1.2);
+                }
+            } else {
                 if (INTERSECTED) INTERSECTED.scale.set(1, 1, 1);
-                INTERSECTED = intersects[0].object;
-                INTERSECTED.scale.set(1.2, 1.2, 1.2);
+                INTERSECTED = null;
             }
-        } else {
-            if (INTERSECTED) INTERSECTED.scale.set(1, 1, 1);
-            INTERSECTED = null;
         }
     }
+
     document.addEventListener('mousemove', onMouseMove, false);
 
     function onDocumentMouseDown(event) {
+        if (isModalOpen) {
+            return; // Ne pas gérer les interactions 3D si la modal est ouverte
+        }
+
         var mouse = new THREE.Vector2();
         var raycaster = new THREE.Raycaster();
         mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
@@ -59,27 +67,62 @@ document.addEventListener('DOMContentLoaded', function () {
         raycaster.setFromCamera(mouse, camera);
         var intersects = raycaster.intersectObjects(scene.children);
 
-        if (intersects.length > 0) {
-            var clickedMesh = intersects[0].object;
+        if (!isModalOpen) {
+            if (intersects.length > 0) {
+                var clickedMesh = intersects[0].object;
 
-            if (clickedMesh === mesh1) {
-                $('#loginModal').modal('show');
-            } else if (clickedMesh === mesh2) {
-                $('#signupModal').modal('show');
+                if (clickedMesh === mesh1 && !isSignupModalOpen) {
+                    $('#loginModal').modal('show');
+                } else if (clickedMesh === mesh2 && !isLoginModalOpen) {
+                    $('#signupModal').modal('show');
+                }
             }
         }
     }
-    document.getElementById('avatar').addEventListener('change', function() {
-        document.getElementById('selectedAvatar').src = this.value;
-    });
 
     document.addEventListener('mousedown', onDocumentMouseDown, false);
 
+    var isModalOpen = false; // Variable pour suivre si une modal est ouverte
+    var isLoginModalOpen = false; // Variable pour suivre si la modal de connexion est ouverte
+    var isSignupModalOpen = false; // Variable pour suivre si la modal d'inscription est ouverte
+
+    // Fonction pour désactiver les contrôles de la caméra lorsque la modal de connexion est ouverte
+    $('#loginModal').on('shown.bs.modal', function () {
+        isModalOpen = true;
+        isLoginModalOpen = true;
+    });
+
+    // Fonction pour réactiver les contrôles de la caméra lorsque la modal de connexion est fermée
+    $('#loginModal').on('hidden.bs.modal', function () {
+        isModalOpen = false;
+        isLoginModalOpen = false;
+    });
+
+    // Fonction pour désactiver les contrôles de la caméra lorsque la modal d'inscription est ouverte
+    $('#signupModal').on('shown.bs.modal', function () {
+        isModalOpen = true;
+        isSignupModalOpen = true;
+    });
+
+    // Fonction pour réactiver les contrôles de la caméra lorsque la modal d'inscription est fermée
+    $('#signupModal').on('hidden.bs.modal', function () {
+        isModalOpen = false;
+        isSignupModalOpen = false;
+    });
+
     function animate() {
         requestAnimationFrame(animate);
-        controls.update();
+
+        if (!isModalOpen) {
+            controls.update();
+        }
+
         renderer.render(scene, camera);
     }
 
     animate();
 });
+
+
+
+
